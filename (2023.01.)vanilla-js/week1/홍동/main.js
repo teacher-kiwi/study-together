1e7a8d874f3b0341eb0ebc412cffeb2622b594ed
nlom0218 <nlom0218@naver.com>
2023-01-03 11:52:57 +0900

    study: #2.2 Variables

--- (2023.01.)vanilla-js/week1/홍동/main.js
+++ (2023.01.)vanilla-js/week1/홍동/main.js
@@ -9,3 +9,23 @@
 // string
 ('hello');
 'hello' + ' my name is hd'; // -> hello my name is hd
+
+// #2.2 Variables
+console.log(5 + 2);
+console.log(5 * 2);
+console.log(5 / 2);
+
+// 변수의 필요성 - 프로그래머들은 게으르다. 더 게으르고 싶어한다.
+// 변수는 값을 저장하거나 유지하는 역할을 한다.
+
+// const - 변하지 않는
+const a = 45;
+const b = 20;
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+
+const myName = 'HD'; // 변수명은 카멜 케이스
+
+console.log(`Hello ${myName}`);
