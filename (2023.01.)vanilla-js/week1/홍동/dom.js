599b67d46dde97c72cde6986c864dee8d5dcadb8
nlom0218 <nlom0218@naver.com>
2023-01-04 16:15:36 +0900

    study: 3.2 Searching For Elements

--- (2023.01.)vanilla-js/week1/홍동/dom.js
+++ (2023.01.)vanilla-js/week1/홍동/dom.js
@@ -4,3 +4,10 @@ document.title = 'Hello! JS!';
 // #3.1 HTML in Javascript
 const title = document.getElementById('title');
 title.innerText += ' Good Luck!';
+
+// #3.2 Searching For Elements
+const contents = document.getElementsByClassName('contents');
+console.dir(contents);
+
+const hello = document.querySelectorAll('div h3:first-child');
+hello.forEach((item) => (item.innerText += ' Oh my god!'));
