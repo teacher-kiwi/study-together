4c03a26085bdb2359ca4e8868d2c5d12d2553e93
nlom0218 <nlom0218@naver.com>
2023-01-05 14:35:39 +0900

    feat: 비디오 재생하기

--- (2023.01.)vanilla-js/week1/홍동/mission.js
+++ (2023.01.)vanilla-js/week1/홍동/mission.js
@@ -1,3 +1,5 @@
+import data from './data.js';
+
 const $topLeftBox = document.querySelector('.top-left');
 const $topRightBox = document.querySelector('.top-right');
 const $bottomLeftBox = document.querySelector('.bottom-left');
@@ -5,16 +7,14 @@ const $bottomRightBox = document.querySelector('.bottom-right');
 const $video = document.querySelector('.video');
 
 function handleMouseEnter(event) {
-  console.log(event);
-  //   $video.innerHTML = `<iframe
-  //         width="1120"
-  //         height="630"
-  //         src="https://www.youtube.com/embed/p_XdZdg9oGc/?"
-  //         title="YouTube video player"
-  //         frameborder="0"
-  //         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-  //         allowfullscreen
-  //       ></iframe>`;
+  const {
+    target: {
+      dataset: { music },
+    },
+  } = event;
+  const { vedio } = data;
+
+  $video.innerHTML = vedio[music];
 }
 
 function handleMouseLeave() {
