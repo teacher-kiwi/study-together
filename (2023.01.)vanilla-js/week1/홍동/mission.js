c4e26f9614073bf6e2984339da31cfeffc9f1349
nlom0218 <nlom0218@naver.com>
2023-01-05 14:13:14 +0900

    feat: dom요소 취득 및 이벤트 등록

---
+++ (2023.01.)vanilla-js/week1/홍동/mission.js
@@ -0,0 +1,14 @@
+const $topLeftBox = document.querySelector('.top-left');
+const $topRightBox = document.querySelector('.top-right');
+const $bottomLeftBox = document.querySelector('.bottom-left');
+const $bottomRightBox = document.querySelector('.bottom-right');
+const $video = document.querySelector('.video');
+
+$topLeftBox.addEventListener('mouseenter', () => {});
+$topLeftBox.addEventListener('mouseleave', () => {});
+$topRightBox.addEventListener('mouseenter', () => {});
+$topRightBox.addEventListener('mouseleave', () => {});
+$bottomLeftBox.addEventListener('mouseenter', () => {});
+$bottomLeftBox.addEventListener('mouseleave', () => {});
+$bottomRightBox.addEventListener('mouseenter', () => {});
+$bottomRightBox.addEventListener('mouseleave', () => {});
