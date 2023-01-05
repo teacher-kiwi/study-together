2130caf83c505283f812f3e778eba2c7938a881c
nlom0218 <nlom0218@naver.com>
2023-01-05 14:16:43 +0900

    feat: handleMouseEnter, handleMouseLeave

--- (2023.01.)vanilla-js/week1/홍동/mission.js
+++ (2023.01.)vanilla-js/week1/홍동/mission.js
@@ -4,11 +4,27 @@ const $bottomLeftBox = document.querySelector('.bottom-left');
 const $bottomRightBox = document.querySelector('.bottom-right');
 const $video = document.querySelector('.video');
 
-$topLeftBox.addEventListener('mouseenter', () => {});
-$topLeftBox.addEventListener('mouseleave', () => {});
-$topRightBox.addEventListener('mouseenter', () => {});
-$topRightBox.addEventListener('mouseleave', () => {});
-$bottomLeftBox.addEventListener('mouseenter', () => {});
-$bottomLeftBox.addEventListener('mouseleave', () => {});
-$bottomRightBox.addEventListener('mouseenter', () => {});
-$bottomRightBox.addEventListener('mouseleave', () => {});
+function handleMouseEnter() {
+  $video.innerHTML = `<iframe
+        width="1120"
+        height="630"
+        src="https://www.youtube.com/embed/p_XdZdg9oGc/?"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>`;
+}
+
+function handleMouseLeave() {
+  $video.innerHTML = ``;
+}
+
+$topLeftBox.addEventListener('mouseenter', handleMouseEnter);
+$topLeftBox.addEventListener('mouseleave', handleMouseLeave);
+$topRightBox.addEventListener('mouseenter', handleMouseEnter);
+$topRightBox.addEventListener('mouseleave', handleMouseEnter);
+$bottomLeftBox.addEventListener('mouseenter', handleMouseEnter);
+$bottomLeftBox.addEventListener('mouseleave', handleMouseEnter);
+$bottomRightBox.addEventListener('mouseenter', handleMouseEnter);
+$bottomRightBox.addEventListener('mouseleave', handleMouseEnter);
