74e29cb789e6f0cd77720f9e0a9d778e77f0ad5c
민트향기 <69835453+mintkaori@users.noreply.github.com>
2023-01-06 15:26:47 +0900

    add Revised Login (form, classlist, localstorage)

--- (2023.01.)vanilla-js/week2/민트향기/js/greetings.js
+++ (2023.01.)vanilla-js/week2/민트향기/js/greetings.js
@@ -1,13 +1,31 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
-function onLoginBtnClick() {
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    if (username === "") {
-        alert("Please write your name");
-    } else if (username.length > 15) {
-        alert("too long!!");
-    }
+    localStorage.setItem(USERNAME_KEY, username);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    paintGreetings(username);
 }
 
-loginButton.addEventListener("click",onLoginBtnClick);
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+loginForm.addEventListener("submit",onLoginSubmit)
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
