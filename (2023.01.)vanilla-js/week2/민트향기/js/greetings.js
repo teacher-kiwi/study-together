69b302ece0bf02022a60d863448c8906ca604280
민트향기 <69835453+mintkaori@users.noreply.github.com>
2023-01-06 14:13:15 +0900

    add Login with alert string length (if~else if)

--- (2023.01.)vanilla-js/week2/민트향기/js/greetings.js
+++ (2023.01.)vanilla-js/week2/민트향기/js/greetings.js
@@ -2,7 +2,12 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-    console.log("Hello",loginInput.value);
+    const username = loginInput.value;
+    if (username === "") {
+        alert("Please write your name");
+    } else if (username.length > 15) {
+        alert("too long!!");
+    }
 }
 
 loginButton.addEventListener("click",onLoginBtnClick);
