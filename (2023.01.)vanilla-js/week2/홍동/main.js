4614af18d4dbe80c751bc75cdd1d6f516bbdfa6d
nlom0218 <nlom0218@naver.com>
2023-01-07 19:24:20 +0900

    study: 4.6 Loading Username

--- (2023.01.)vanilla-js/week2/홍동/main.js
+++ (2023.01.)vanilla-js/week2/홍동/main.js
@@ -3,15 +3,27 @@ const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
 
 const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
 
 function handleSubmitLoginBtn(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
 
   const username = loginInput.value;
-  localStorage.setItem('username', username);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings();
+}
+
+function paintGreetings(username) {
   greeting.innerText = `반가워요! ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener('submit', handleSubmitLoginBtn);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (!savedUsername) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener('submit', handleSubmitLoginBtn);
+} else {
+  paintGreetings(savedUsername);
+}
