7817231839936801c3881c3b7c5e526f3d73f58c
민트향기 <69835453+mintkaori@users.noreply.github.com>
2023-01-07 21:41:14 +0900

    add quotes (using arrays and Math function)

---
+++ (2023.01.)vanilla-js/week2/민트향기/js/quotes.js
@@ -0,0 +1,50 @@
+const quotes = [
+    {
+        quote: "천리길도 한 걸음부터",
+        author: "한국 속담",
+    },
+    { 
+        quote: "삶이 있는 한 희망은 있다",
+        author: "키케로",
+    },
+    {
+        quote: "산다는것 그것은 치열한 전투이다.",
+        author: "로망로랑",
+    },
+    {
+        quote: "하루에 3시간을 걸으면 7년 후에 지구를 한바퀴 돌 수 있다.",
+        author: "사무엘존슨",
+    },
+    {
+        quote: "언제나 현재에 집중할수 있다면 행복할것이다.",
+        author: "파울로 코엘료",
+    },
+    {
+        quote: "진정으로 웃으려면 고통을 참아야하며 , 나아가 고통을 즐길 줄 알아야 해",
+        author: "찰리 채플린",
+    },
+    {
+        quote: "직업에서 행복을 찾아라. 아니면 행복이 무엇인지 절대 모를 것이다",
+        author: "엘버트 허버드",
+    },
+    {
+        quote: "신은 용기있는자를 결코 버리지 않는다.",
+        author: "켄러",
+    },
+    {
+        quote: "피할수 없으면 즐겨라.",
+        author: "로버트 엘리엇",
+    },
+    {
+        quote: "단순하게 살아라. 현대인은 쓸데없는 절차와 일 때문에 얼마나 복잡한 삶을 살아가는가?",
+        author: "이드리스 샤흐",
+    },
+];
+
+const quote = document.querySelector(".quote span:first-child");
+const author = document.querySelector(".quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
