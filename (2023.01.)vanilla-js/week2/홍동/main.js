abc4db96e16f24abb77aa31a1d47a4463f1e6467
nlom0218 <nlom0218@naver.com>
2023-01-07 17:43:05 +0900

    study: 4.4 Getting Username

--- (2023.01.)vanilla-js/week2/홍동/main.js
+++ (2023.01.)vanilla-js/week2/홍동/main.js
@@ -1,10 +1,16 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
+
+const HIDDEN_CLASSNAME = 'hidden';
 
 function handleSubmitLoginBtn(event) {
   event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+
   const username = loginInput.value;
-  console.log(username);
+  greeting.innerText = `반가워요! ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener('submit', handleSubmitLoginBtn);
