2ac6879e04e8e152fc3dd530b819e9e743ad002c
nlom0218 <nlom0218@naver.com>
2023-01-08 19:15:40 +0900

    study: 5.1 Timeouts and Dates

--- (2023.01.)vanilla-js/week2/홍동/js/clock.js
+++ (2023.01.)vanilla-js/week2/홍동/js/clock.js
@@ -1,7 +1,9 @@
 const clock = document.querySelector('#clock');
 
-function sayHello() {
-  console.log('hello');
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setInterval(sayHello, 2000);
+getClock();
+setInterval(getClock, 1000);
