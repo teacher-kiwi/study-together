33d0bd5ec23de6e9f6bfc6eaeb9c7fef1267b9e0
민트향기 <69835453+mintkaori@users.noreply.github.com>
2023-01-08 17:49:23 +0900

    add weather(no active)

---
+++ (2023.01.)vanilla-js/week2/민트향기/js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "3c2ea7ac67710ac4e468ffa01235fb44"
+
+function onGeoOK(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+    alert("Error")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOK,onGeoError);
