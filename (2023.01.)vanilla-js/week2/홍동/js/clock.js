2b98bf52a75de6b9b063ad9939c75e53a55d8f5c
nlom0218 <nlom0218@naver.com>
2023-01-08 19:24:03 +0900

    study: 5.2 PadStart

--- (2023.01.)vanilla-js/week2/홍동/js/clock.js
+++ (2023.01.)vanilla-js/week2/홍동/js/clock.js
@@ -2,7 +2,14 @@ const clock = document.querySelector('#clock');
 
 function getClock() {
   const date = new Date();
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const hours = setPadStart(date.getHours());
+  const minutes = setPadStart(date.getMinutes());
+  const seconds = setPadStart(date.getSeconds());
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+function setPadStart(number) {
+  return String(number).padStart(2, '0');
 }
 
 getClock();
