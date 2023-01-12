a6a9aa3a8f5e8718543a10ffe2f2706673a707de
nlom0218 <nlom0218@naver.com>
2023-01-12 12:02:24 +0900

    feat: clock 년,월,일,요일 추가하기

--- (2023.01.)vanilla-js/week2/홍동/js/clock.js
+++ (2023.01.)vanilla-js/week2/홍동/js/clock.js
@@ -1,10 +1,33 @@
-const clock = document.querySelector('#clock');
+import { $ } from './libs/dom.js';
+
+let week = [
+  '일요일',
+  '월요일',
+  '화요일',
+  '수요일',
+  '목요일',
+  '금요일',
+  '토요일',
+];
 
 function getClock() {
   const date = new Date();
+  getTime(date);
+  getDay(date);
+}
+
+function getTime(date) {
   const hours = setPadStart(date.getHours());
   const minutes = setPadStart(date.getMinutes());
-  clock.innerText = `${hours}:${minutes}`;
+  $('.clock-time').innerText = `${hours}:${minutes}`;
+}
+
+function getDay(date) {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const dateNum = date.getDate();
+  const day = week[date.getDay()];
+  $('.clock-day').innerText = `${year}년 ${month}월 ${dateNum}일 ${day}`;
 }
 
 function setPadStart(number) {
