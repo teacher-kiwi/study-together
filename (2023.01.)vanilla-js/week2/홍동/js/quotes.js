befc078aad5636bee879336c2ab80ef6d63c0d24
nlom0218 <nlom0218@naver.com>
2023-01-12 11:07:36 +0900

    feat: 명언 수정 및 dom 셀렉터 분리하기

--- (2023.01.)vanilla-js/week2/홍동/js/quotes.js
+++ (2023.01.)vanilla-js/week2/홍동/js/quotes.js
@@ -1,52 +1,8 @@
-const quotes = [
-  {
-    quote: 'The way to get started is to quit talking and begin doing.',
-    author: 'Walt Disney',
-  },
-  {
-    quote: "Life is what happens when you're busy making other plans.",
-    author: 'John Lennon',
-  },
-  {
-    quote:
-      'The world is a book and those who do not travel read only one page.',
-    author: 'Saint Augustine',
-  },
-  {
-    quote: 'Life is either a daring adventure or nothing at all.',
-    author: 'Helen Keller',
-  },
-  {
-    quote: 'To Travel is to Live',
-    author: 'Hans Christian Andersen',
-  },
-  {
-    quote: 'Only a life lived for others is a life worthwhile.',
-    author: 'Albert Einstein',
-  },
-  {
-    quote: 'You only live once, but if you do it right, once is enough.',
-    author: 'Mae West',
-  },
-  {
-    quote: 'Never go on trips with anyone you do ntot love.',
-    author: 'Hemmingway',
-  },
-  {
-    quote: 'We wander for distraction, but we travel for fulfilment.',
-    author: 'Hilaire Belloc',
-  },
-  {
-    quote: 'Travel expands the mind and fills the gap.',
-    author: 'Sheda Savage',
-  },
-];
-
-const $quote = document.querySelector('#quote span:first-child');
-const $author = document.querySelector('#quote span:last-child');
+import { $ } from './libs/dom.js';
+import quotes from './libs/quotes.js';
 
 const randomNum = Math.floor(Math.random() * quotes.length);
 const { quote, author } = quotes[randomNum];
 
-$quote.innerText = quote;
-$author.innerText = author;
+$('#quote span:first-child').innerText = quote;
+$('#quote span:last-child').innerText = author;
