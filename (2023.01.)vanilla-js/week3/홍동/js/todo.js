dc82e0b4daab04056522945389b86e556daa6660
nlom0218 <nlom0218@naver.com>
2023-01-14 23:21:54 +0900

    study: 7.1Adding ToDos

--- (2023.01.)vanilla-js/week3/홍동/js/todo.js
+++ (2023.01.)vanilla-js/week3/홍동/js/todo.js
@@ -2,10 +2,18 @@ import { $ } from './libs/dom.js';
 
 const $todoInput = $('#todo-form input');
 
+function paintTodo(todo) {
+  $('#todo-list').insertAdjacentHTML(
+    'beforeend',
+    `<li><span>${todo}</span></li>`
+  );
+}
+
 function handleSubmitTodo(event) {
   event.preventDefault();
   const newTodo = $todoInput.value;
   $todoInput.value = '';
+  paintTodo(newTodo);
 }
 
 $('#todo-form').addEventListener('submit', handleSubmitTodo);
