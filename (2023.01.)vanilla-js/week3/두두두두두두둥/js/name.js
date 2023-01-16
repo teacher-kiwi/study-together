8f0fcaa7d0478fa6f3e6370cfdd6297e7d7a504b
doodoodoong <doo_doong@hanmail.net>
2023-01-16 14:29:57 +0900

    setting

---
+++ (2023.01.)vanilla-js/week3/두두두두두두둥/js/name.js
@@ -0,0 +1,26 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const savedUsername = localStorage.getItem("username");
+const windowName = document.querySelector("#name");
+
+function onLoginBtn(event) {
+  event.preventDefault();
+  loginForm.classList.add("hidden");
+  const username = loginInput.value;
+  localStorage.setItem("username", username);
+  paintGreeting(username);
+}
+
+function paintGreeting(username) {
+  loginForm.classList.add("hidden");
+  windowName.classList.remove("hidden");
+  windowName.classList.add("fade-in");
+  windowName.innerText = `Hello ${username}`;
+}
+
+if (savedUsername == null) {
+  loginForm.classList.remove("hidden");
+  loginForm.addEventListener("submit", onLoginBtn);
+} else {
+  paintGreeting(savedUsername);
+}
