45492f401e258c3b8b208f240869c5dc85695661
nlom0218 <nlom0218@naver.com>
2023-01-17 19:15:58 +0900

    study: 8.0 Weather API

--- (2023.01.)vanilla-js/week3/홍동/js/weather.js
+++ (2023.01.)vanilla-js/week3/홍동/js/weather.js
@@ -1,5 +1,17 @@
-function onGeoOk({ coords: { latitude: lat, longitude: lng } }) {
-  console.log(lat, lng);
+import { $ } from './libs/dom.js';
+
+const API_KEY = '58f5d487b06fd096821ef170a3d65d47';
+
+function onGeoOk({ coords: { latitude: lat, longitude: lon } }) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((res) => res.json())
+    .then(({ name, weather }) => {
+      $('#weather').insertAdjacentHTML(
+        'beforeend',
+        `<span>${weather[0].main}</span>`
+      );
+    });
 }
 
 function onGeoError() {
